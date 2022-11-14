fb834c3cfd2c18ba15d668ef138c5f58034ca93d
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-11-14 15:41:42 -0300

    Activity views logic

--- app/src/main/java/livrokotlin/com/br/MainActivity.kt
+++ app/src/main/java/livrokotlin/com/br/MainActivity.kt
@@ -1,11 +1,25 @@
 package livrokotlin.com.br
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Ligando p.Adpter para o nosso componente de layout
+        val produtosAdpter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+        list_view_produtos.adapter = produtosAdpter
+
+        //Interação do botão para add na lista
+        btn_inserir.setOnClickListener {
+            val produto = txt_produto.text.toString()
+            produtosAdpter.add(produto)
+        }
+
+
     }
 }
